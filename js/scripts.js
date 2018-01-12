735380d409793e69e3bbfddbd2ea2e653d4028db
Kayla Ondracek <kayla.renee@gmail.com>
2018-01-12 10:51:21 -0800

    add one more question and corresponding logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,29 @@
+
 $(document).ready(function() {
   $("form#track-quiz").submit(function(event) {
     var name = $("#name").val();
     var companyInput = $("input:radio[name=company]:checked").val();
     var osInput = $("input:radio[name=os]:checked").val();
+    var scenarioInput = $("input:radio[name=scenario]:checked").val();
     var mobileInput = $("#mobile").val();
     var interfaceInput = $("#interface").val();
-    console.log(name+companyInput+osInput+mobileInput+interfaceInput);
+
+    console.log(name+", "+companyInput+", "+osInput+", "+scenarioInput+", "+mobileInput+", "+interfaceInput);
+
     //hide previous result
     $(".result").hide();
     $(".thanks").remove();
 
+    //add "thanks" to result set
     $(".results").prepend("<p class='thanks'>Thanks, "+name+"! You should study...");
 
-    if (interfaceInput === "Yes") {
+    if (scenarioInput === "dunno") {
+      $("#dunno").show();
+    } else if (interfaceInput !== "No" && scenarioInput === "design") {
       $("#design").show();
-    } else if ((mobileInput === "Yes" || mobileInput === "Maybe") && osInput === "android") {
+    } else if ((mobileInput === "Yes" || mobileInput === "Maybe") && osInput === "android" && scenarioInput === "mobile") {
       $("#java").show();
-    } else if ((companyInput === "large" || companyInput === "medium") && (osInput === "windows" || osInput === "dunno")) {
+    } else if ((companyInput === "large" || companyInput === "medium") && (osInput === "windows" || osInput === "dunno") && (scenarioInput === "csharp" || scenarioInput === "ruby" || scenarioInput === php)) {
       $("#csharp").show();
     } else if (companyInput === "design" || interfaceInput !== "No") {
       $("#design").show();
@@ -24,6 +31,7 @@ $(document).ready(function() {
       $("#ruby").show();
     } else {
       $("#dunno").show();
+      $("#other").show();
     }
     event.preventDefault();
   });
