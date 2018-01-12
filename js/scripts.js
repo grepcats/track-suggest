76974d72d07bf69fa203107f80db6c85872c406b
Kayla Ondracek <kayla.renee@gmail.com>
2018-01-12 10:17:36 -0800

    add another question, more logic to accommodate question

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,24 @@ $(document).ready(function() {
     var companyInput = $("input:radio[name=company]:checked").val();
     var osInput = $("input:radio[name=os]:checked").val();
     var mobileInput = $("#mobile").val();
-    //console.log(name+companyInput+osInput+mobileInput);
+    var interfaceInput = $("#interface").val();
+    console.log(name+companyInput+osInput+mobileInput+interfaceInput);
     //hide previous result
     $(".result").hide();
     $(".thanks").remove();
 
     $(".results").prepend("<p class='thanks'>Thanks, "+name+"! You should study...");
 
-    if ((companyInput === "large" || companyInput === "medium") && (osInput === "windows" || osInput === "dunno") && mobileInput === "No") {
+    if (interfaceInput === "Yes") {
+      $("#design").show();
+    } else if ((mobileInput === "Yes" || mobileInput === "Maybe") && osInput === "android") {
+      $("#java").show();
+    } else if ((companyInput === "large" || companyInput === "medium") && (osInput === "windows" || osInput === "dunno")) {
       $("#csharp").show();
+    } else if (companyInput === "design" || interfaceInput !== "No") {
+      $("#design").show();
     } else if ((companyInput === "small" || companyInput === "medium") && osInput !== "windows" && mobileInput !== "Yes") {
       $("#ruby").show();
-    } else if (companyInput === "design") {
-      $("#design").show();
     } else {
       $("#dunno").show();
     }
