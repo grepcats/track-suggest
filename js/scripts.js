55c0acf79c1c37e077bd828bfad5fbe1acec091b
Kayla Ondracek <kayla.renee@gmail.com>
2018-01-12 10:03:55 -0800

    add results to quiz, some initial logic to submit event in the js

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,25 @@ $(document).ready(function() {
     var companyInput = $("input:radio[name=company]:checked").val();
     var osInput = $("input:radio[name=os]:checked").val();
     var mobileInput = $("#mobile").val();
-    console.log(name+companyInput+osInput+mobileInput);
+    //console.log(name+companyInput+osInput+mobileInput);
+    //hide previous result
+    $(".result").hide();
+    $(".thanks").remove();
 
+    $(".results").prepend("<p class='thanks'>Thanks, "+name+"! You should study...");
+
+    if ((companyInput === "large" || companyInput === "medium") && (osInput === "windows" || osInput === "dunno") && mobileInput === "No") {
+      $("#csharp").show();
+    } else if ((companyInput === "small" || companyInput === "medium") && osInput !== "windows" && mobileInput !== "Yes") {
+      $("#ruby").show();
+    } else if (companyInput === "design") {
+      $("#design").show();
+    } else {
+      $("#dunno").show();
+    }
     event.preventDefault();
   });
+
 
 
 });
